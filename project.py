ec6016fb9055aeca410bdb966f96a4c9fe475ae5
vagrant <vagrant@vagrant.vm>
2018-09-05 16:35:07 +0000

    Adds Route Decorations For Add, Edit, Delete pages

--- project.py
+++ project.py
@@ -12,9 +12,9 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     output = ''
     for i in items:
@@ -25,6 +25,18 @@ def HelloWorld():
         output += i.description
         output += '<br><br>'
     return output
+
+@app.route('/restaurant/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
